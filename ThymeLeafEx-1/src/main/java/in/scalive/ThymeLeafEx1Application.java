019c023bcb064b759732357e1226463cf480b5c8
Manu Kumar <143504163+Manukumar7644@users.noreply.github.com>
2026-04-27 17:30:18 +0530

    Add files via upload

---
+++ ThymeLeafEx-1/src/main/java/in/scalive/ThymeLeafEx1Application.java
@@ -0,0 +1,13 @@
+package in.scalive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeLeafEx1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeLeafEx1Application.class, args);
+	}
+
+}
---
+++ ThymeLeafEx-1/src/main/java/in/scalive/controller/HelloController.java
@@ -0,0 +1,17 @@
+package in.scalive.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+
+@Controller
+public class HelloController {
+   @RequestMapping(method = RequestMethod.GET, value="/hello")
+	public String sayHello(Model model) {
+	   model.addAttribute("message","Hello User, Welcome To Thymeleaf");
+	   return "greetings";
+   }
+}
