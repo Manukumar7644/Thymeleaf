98d0fc008dfa65e27bd3b0847e0eaaf04f6cf132
Manu Kumar <143504163+Manukumar7644@users.noreply.github.com>
2026-04-29 08:37:47 +0530

    Add files via upload

---
+++ ThymeLeafEx-3/src/main/java/in/scalive/ThymeLeafEx3Application.java
@@ -0,0 +1,13 @@
+package in.scalive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeLeafEx3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeLeafEx3Application.class, args);
+	}
+
+}
---
+++ ThymeLeafEx-3/src/main/java/in/scalive/controller/StudentController.java
@@ -0,0 +1,25 @@
+package in.scalive.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import in.scalive.model.Student;
+
+
+
+@Controller
+public class StudentController {
+   @RequestMapping(method = RequestMethod.GET, value="/showStudent")
+   public String showStudent(Model model) {
+	   Student student=new Student();
+	   student.setId(101);
+	   student.setName("Manish");
+	   student.setCourse("Spring Boot");
+	   model.addAttribute("student", student);
+	   return "show-student";
+	   
+   }
+	
+}
