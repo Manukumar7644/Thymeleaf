65766b543a3943c79ad37856738bca2b1da4bbf0
Manu Kumar <143504163+Manukumar7644@users.noreply.github.com>
2026-05-02 16:14:47 +0530

    Add files via upload

---
+++ ThymeLeafEx-7/src/main/java/in/scalive/ThymeLeafEx7Application.java
@@ -0,0 +1,13 @@
+package in.scalive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeLeafEx7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeLeafEx7Application.class, args);
+	}
+
+}
