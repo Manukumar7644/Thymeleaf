eea80440678339e1206c6e3b1bb110693ae588e1
Manu Kumar <143504163+Manukumar7644@users.noreply.github.com>
2026-05-06 23:53:16 +0530

    Add files via upload

---
+++ ThymeLeafEx-11/src/main/java/in/scalive/controller/StudentController.java
@@ -0,0 +1,29 @@
+package in.scalive.controller;
+
+import java.util.List;
+
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import in.scalive.model.Student;
+
+
+
+@Controller
+public class StudentController {
+   @RequestMapping(method=RequestMethod.GET,value="/showcourses")	
+   public String showCourses(Model model) {
+	   String[] courses= {"Java","Python","Spring Boot"};
+	   model.addAttribute("courses",courses);
+	   return "show-courses";
+   }
+   @RequestMapping(method=RequestMethod.GET,value="/showstudents")	
+   public String showStudents(Model model) {
+	   List <Student>studList=List.of(new Student(101,"Ravi","Java"),new Student(102,"Ajay","Python"),new Student(103,"Deepak","Ruby"));
+	   model.addAttribute("students",studList);
+	   return "show-students";
+   }
+}
